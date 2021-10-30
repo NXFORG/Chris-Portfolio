9465c9aef8cdbd2c1dcb063b92721dc90749a575
“nxforg” <christopher.anstead@yahoo.co.uk>
2021-10-30 11:48:10 +0100

    add stack hover event listeners

---
+++ static/js/github.js
@@ -0,0 +1,18 @@
+const getGithubUserData = async (username) => {
+    try {
+        let resp = await fetch(`https://api.github.com/users/${username}`);
+        let data = await resp.json();
+        console.log(data)
+        if(data){
+            document.getElementById('githubAvatar').src = data.avatar_url;
+            document.getElementById('githubUsername').textContent = data.login;
+            document.getElementById('githubRepositories').textContent = data.public_repos;
+            document.getElementById('githubMemberYear').textContent = data.created_at.substring(0,4);
+        }else {
+            console.log('no data found');
+        }
+    } catch (err){
+        console.error(`${err}`);
+    }
+}
+//getGithubUserData('nxforg');
