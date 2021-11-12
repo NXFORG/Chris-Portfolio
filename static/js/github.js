d6fff9cc2628d5b1a576a76cce685bb66c84ddea
“nxforg” <christopher.anstead@yahoo.co.uk>
2021-11-12 12:55:14 +0000

    add most used language to github stats box

--- static/js/github.js
+++ static/js/github.js
@@ -1,16 +1,54 @@
-const getGithubUserData = async (username) => {
+const callAPI = async (route, value) => {
     try {
-        let resp = await fetch(`https://api.github.com/users/${username}`);
-        let data = await resp.json();
-        if(data){
-            document.getElementById('githubUsername').textContent = data.login;
-            document.getElementById('githubRepositories').textContent = data.public_repos;
-            document.getElementById('githubMemberYear').textContent = data.created_at.substring(0,4);
-        }else {
-            console.log('no data found');
-        }
+        let resp = await fetch(`https://api.github.com/${route}/${value}`);
+        let data = [];
+        data = await resp.json();
+        return data;
     } catch (err){
         console.error(`${err}`);
     }
 }
+
+const getGithubUserData = async (username) => {
+    try {
+        let data = await callAPI('users', username);
+        if(data){
+            document.getElementById('githubUsername').textContent = data.login;
+            document.getElementById('githubRepositories').textContent = data.public_repos;
+            document.getElementById('githubMemberYear').textContent = data.created_at.substring(0,4);
+        } else {
+            console.log('No data found');
+        }
+    } catch(err) {
+        console.log(`Error: ${err}`);
+    }
+}
 getGithubUserData('nxforg');
+
+const getGithubLanguages = async (username) => {
+    try {
+        let repos = await callAPI('users', `${username}/repos`);
+        if(repos){
+            let totalFound = 1;
+            let found = 0;
+            let find;
+            for (let i = 0; i < repos.length; i++){
+                for (let j = i; j < repos.length; j++){
+                    repos[i].language === repos[j].language && found++;
+                    if (totalFound < found){
+                        totalFound = found; 
+                        find = repos[i].language;
+                    }
+                }
+                found = 0;
+            }
+            document.getElementById('languageCount').textContent = `My most used language is currently ${find}. It was the main language used in ${totalFound} of my repositories.`;
+        } else {
+            console.log('No data found');
+        }
+    } catch(err) {
+        console.log(`Error: ${err}`);
+    }
+}
+
+getGithubLanguages('nxforg');
